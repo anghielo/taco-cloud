ba881a5aa540457b537d9fa3ed09d5e8aff1b7f1
anghielo <user_noreply@github.com>
2021-09-06 16:23:55 -0700

    update

---
+++ src/main/java/com/paradigm_shifters/tacocloud/model/Ingredient.java
@@ -0,0 +1,15 @@
+package com.paradigm_shifters.tacocloud.model;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class Ingredient {
+    private final String id;
+    private final String name;
+    private final Type type;
+    public static enum Type {
+        WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+    }
+}
