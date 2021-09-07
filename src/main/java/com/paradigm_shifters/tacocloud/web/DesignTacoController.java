5156c1a091b99d86801e5de5676165e2eddf40d4
anghielo <user_noreply@github.com>
2021-09-06 17:32:04 -0700

    update

---
+++ src/main/java/com/paradigm_shifters/tacocloud/web/DesignTacoController.java
@@ -0,0 +1,51 @@
+package com.paradigm_shifters.tacocloud.web;
+
+import lombok.extern.slf4j.Slf4j;
+import com.paradigm_shifters.tacocloud.model.Taco;
+import com.paradigm_shifters.tacocloud.model.Ingredient;
+import com.paradigm_shifters.tacocloud.model.Ingredient.Type;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
+@Slf4j          // Allows end user to plug in the desired logging framework at deployment time
+@Controller     // When applied at class level, it specifies that the Controller class will handle REST requests
+@RequestMapping("/design")
+public class DesignTacoController {
+
+    @GetMapping
+    public String showDesignForm(Model model) {
+        List<Ingredient> ingredients = Arrays.asList(
+                new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
+                new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
+                new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
+                new Ingredient("CARN", "Carnitas", Type.PROTEIN),
+                new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
+                new Ingredient("LETC", "Lettuce", Type.VEGGIES),
+                new Ingredient("CHED", "Cheddar", Type.CHEESE),
+                new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
+                new Ingredient("SLSA", "Salsa", Type.SAUCE),
+                new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
+        );
+
+        Type[] types = Type.values();
+        for (Type type : types) {
+            model.addAttribute(type.toString().toLowerCase(),
+                    filterByType(ingredients, type));
+        }
+
+        model.addAttribute("design", new Taco());
+
+        return "design";
+    }
+}
