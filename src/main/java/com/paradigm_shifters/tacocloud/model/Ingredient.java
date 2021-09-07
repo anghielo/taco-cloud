8ed03d4bad0327b788e6cd48b993f6e92a39fe52
anghielo <user_noreply@github.com>
2021-09-06 18:05:44 -0700

    Update

--- src/main/java/com/paradigm_shifters/tacocloud/model/Ingredient.java
+++ src/main/java/com/paradigm_shifters/tacocloud/model/Ingredient.java
@@ -10,6 +10,7 @@ public class Ingredient {
     private final String id;
     private final String name;
     private final Type type;
+
     public enum Type {
         WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
     }
