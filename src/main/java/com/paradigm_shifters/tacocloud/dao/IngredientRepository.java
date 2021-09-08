9107f13ca8f19bba89ce1724c90d87c06bec350a
anghielo <user_noreply@github.com>
2021-09-08 02:27:35 -0700

    Update

--- src/main/java/com/paradigm_shifters/tacocloud/dao/IngredientRepository.java
+++ src/main/java/com/paradigm_shifters/tacocloud/dao/IngredientRepository.java
@@ -1,4 +1,13 @@
 package com.paradigm_shifters.tacocloud.dao;
 
+import com.paradigm_shifters.tacocloud.model.Ingredient;
+
+// Implementation of this interface repository will use the JdbcTemplate to query database.
 public interface IngredientRepository {
+
+    Iterable<Ingredient> findAll();
+
+    Ingredient findOne(String id);
+
+    Ingredient save(Ingredient ingredient);
 }
