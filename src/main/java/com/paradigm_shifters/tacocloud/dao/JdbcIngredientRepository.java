b439d694ea8890d57b6da95b42ba210007619794
anghielo <user_noreply@github.com>
2021-09-08 02:46:02 -0700

    Update

--- src/main/java/com/paradigm_shifters/tacocloud/dao/JdbcIngredientRepository.java
+++ src/main/java/com/paradigm_shifters/tacocloud/dao/JdbcIngredientRepository.java
@@ -9,14 +9,22 @@ import org.springframework.stereotype.Repository;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-
+/*
+  The @Repository includes @Controller and @Component declaring that this
+  class should be automatically discovered by Spring component scanning
+  and instantiated as a bean in the Spring application context. When
+  Spring creates the class bean, it injects it with JdbcTemplate via
+  the @Autowired annotated construction. The constructor assigns
+  JdbcTemplate to an instance variable that will be used in other
+  methods to query and insert into the database.
+ */
 @Repository
 public class JdbcIngredientRepository
         implements IngredientRepository {
 
     private JdbcTemplate jdbc;
 
-    @Autowired
+    @Autowired             // Autowire one primary constructor, not both otherwise set one @Autowired as true and the other as false
     public JdbcIngredientRepository(JdbcTemplate jdbc) {
         this.jdbc = jdbc;
     }
@@ -34,16 +42,21 @@ public class JdbcIngredientRepository
                 this::mapRowToIngredient, id);
     }
 
-    @Override
-    public Ingredient save(Ingredient ingredient) {
-        return null;
-    }
-
     private Ingredient mapRowToIngredient(ResultSet rs, int rowNum)
             throws SQLException {
         return new Ingredient(
                 rs.getString("id"),
                 rs.getString("name"),
                 Ingredient.Type.valueOf(rs.getString("type")));
+    }
+
+    @Override
+    public Ingredient save(Ingredient ingredient) {
+        jdbc.update(
+                "insert into Ingredient (id, name, type) values (?, ?, ?)",
+                ingredient.getId(),
+                ingredient.getName(),
+                ingredient.getType().toString());
+        return ingredient;
     }
 }
